4b1462a30ff1db1c63a1fce5b4797d3bd54280bd
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-06-13 12:53:37 +0700

    create destination image table

---
+++ database/migrations/2021_06_13_055209_create_destination_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDestinationImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destination_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('destination_id');
+            $table->text('image');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destination_images');
+    }
+}
