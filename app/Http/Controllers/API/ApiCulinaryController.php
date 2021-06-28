0b9c193dcc9d5cccf00058d829462bfd74ae03c9
hafiyyanabdulaziz <hafiyyan.123@gmail.com>
2021-06-28 10:09:54 +0700

    perbaikan sedikit pada tampilan admin dan memperbaiki random api

--- app/Http/Controllers/API/ApiCulinaryController.php
+++ app/Http/Controllers/API/ApiCulinaryController.php
@@ -12,7 +12,7 @@ class ApiCulinaryController extends Controller
 {
     public function random()
     {
-        $item = Culinary::with('images')->get()->random(5);
+        $item = Culinary::with('images')->get()->random(3);
         try {
             return ResponseFormatter::success([
                 'item' => $item,
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,7 +30,8 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center"
+                href="{{ route('admin.dashboard') }}">
                 <div class="sidebar-brand-icon">
                     <img src="assets/landing/images/favicon.ico" alt="">
                 </div>
@@ -44,7 +45,7 @@
             <li class="nav-item active">
                 <a class="nav-link" href="{{ route('admin.dashboard') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>Coming Soon</span></a>
             </li>
 
             <!-- Divider -->
@@ -54,7 +55,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('admin.dashboard') }}">
                     <i class="fas fa-users"></i>
-                    <span>Users</span></a>
+                    <span>Coming Soon</span></a>
             </li>
 
             <!-- Divider -->
